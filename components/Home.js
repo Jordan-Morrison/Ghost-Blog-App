fea946a0115df1e5c4163fe91cf3a1e6ded31fdc
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-10-10 17:08:52 -0400

    Download html + images
    You can now download HTML and images to be loaded locally. This is currently using base64

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import * as FileSystem from 'expo-file-system';
 
 export default function Home(props) {
 
@@ -16,11 +17,63 @@ export default function Home(props) {
         setPosts(data.posts);
     }
 
+    async function downloadPost(uuid, html) {
+        let imageTagRegex = new RegExp('<img(.+?)src\s*=\s*\\"(.+?)\\"', "g");
+        
+        let imageTags = html.match(imageTagRegex);
+
+        let imagesToDownload = await imageTags.map(tag => {
+            let url = tag.match(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g)[0];
+            return {
+                url: url,
+                name: url.match(/[^/\\&\?]+\.\w{3,4}(?=([\?&].*$|$))/g)[0]
+            }
+        });
+
+        imagesToDownload = [...new Set(imagesToDownload)];
+
+        for (let image in imagesToDownload){
+            image = imagesToDownload[image];
+
+            let fileName = `${imagesToDownload.indexOf(image)}-${image.name}`;
+            let downloaded = await FileSystem.downloadAsync(image.url, FileSystem.documentDirectory + fileName).catch(err => {
+                console.error(err);
+            });
+            if (downloaded.uri){
+                // imagesToDownload[imagesToDownload.indexOf(image)].path = downloaded.uri;
+
+                html = html.split(image.url).join("data:image/gif;base64, " + await FileSystem.readAsStringAsync(downloaded.uri, {
+                    encoding: FileSystem.EncodingType.Base64
+                }));
+
+            }
+        }
+
+        console.log(html);
+        console.log(imagesToDownload);
+
+        FileSystem.writeAsStringAsync(`${FileSystem.documentDirectory}post.html`, html).catch(err => {
+            console.error(err);
+        });
+
+        props.navigation.navigate("Episode", {content: await FileSystem.readAsStringAsync(`${FileSystem.documentDirectory}post.html`), title: "PLZ WORK"})
+
+        // FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + uuid).catch(err => {
+        //     console.log("Directory already exists");
+        // });
+        
+        // FileSystem.writeAsStringAsync(`${FileSystem.documentDirectory}${uuid}/post.html`, html).catch(err => {
+        //     console.error(err);
+        // });
+
+        console.log(await FileSystem.readDirectoryAsync(FileSystem.documentDirectory));
+    }
+
     function ListItem(post) {
         return (
             <TouchableOpacity style={styles.listItem} onPress={() => {props.navigation.navigate("Episode", {content: post.html, title: post.title})}}>
                 <Text>{post.title}</Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => {downloadPost(post.uuid, post.html)}}>
                     <Ionicons style={styles.downloadIcon} name="ios-add" size={25}/>
                 </TouchableOpacity>
             </TouchableOpacity>
@@ -29,7 +82,7 @@ export default function Home(props) {
 
   return (
     <View style={styles.container}>
-        <FlatList style={styles.list} data={posts} renderItem={({item}) => <ListItem title={item.title} html={item.html} />} keyExtractor={item => item.id}/>
+        <FlatList style={styles.list} data={posts} renderItem={({item}) => <ListItem title={item.title} html={item.html} uuid={item.uuid} />} keyExtractor={item => item.uuid}/>
     </View>
 
   );
