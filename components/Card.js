161863a20c929cea8812e3e36f1b8f94367889fd
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-11-01 13:33:29 -0400

    Updated Card Styling
    The card styles are now updated with a much cleaner design

---
+++ components/Card.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import {downloadPost} from './helpers/Downloader';
+
+export default function Card(props) {
+
+  return (
+    <TouchableOpacity style={styles.card} onPress={() => {props.navigation.navigate("Episode", {content: props.post.html, title: props.post.title})}}>
+        <View style={{borderTopRightRadius: 10, borderTopLeftRadius: 10, overflow: "hidden"}}>
+        <Image style={styles.featuredImage} source={{uri: props.post.feature_image}}/>
+        </View>
+        <View style={styles.contentView}>
+            <View style={styles.tagAndDownloadBar}>
+                <Text style={styles.primaryTag}>{props.post.primary_tag ? props.post.primary_tag.name : "gg"}</Text>
+                <TouchableOpacity onPress={() => {downloadPost(props.post)}}>
+                    <Ionicons style={styles.downloadIcon} name="ios-add" size={25}/>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.title}>{props.post.title}</Text>
+            <Text style={styles.description}>{props.post.excerpt}</Text>
+            <Text style={styles.timeText}>6 days ago &bull; 2 min read</Text>
+        </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        display: "flex",
+        marginHorizontal: 20,
+        marginVertical: 10,
+        shadowColor: 'rgba(0, 0, 0, 0.1)',
+        shadowOffset: { width: 0, height: 15 },
+        shadowOpacity: 1,
+        shadowRadius: 10
+    },
+    featuredImage: {
+        width: "100%",
+        height: 200,
+        overflow: "hidden",
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10
+    },
+    contentView: {
+        backgroundColor: "#F4F5F5",
+        padding: 10,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+    },
+    tagAndDownloadBar: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    primaryTag: {
+        fontSize: 14,
+        flexGrow: 1
+    },
+    title: {
+        fontWeight: "500",
+        fontSize: 19,
+        lineHeight: 26,
+        paddingVertical: 5
+    },
+    description: {
+        fontSize: 14,
+        paddingVertical: 5
+    },
+    timeText: {
+        fontSize: 12,
+        paddingVertical: 5
+    },
+    downloadIcon: {
+        // paddingHorizontal: 20,
+        // paddingVertical: 20
+    }
+});
+
+
+// const styles = StyleSheet.create({
+//     listItem: {
+//         borderRadius: 4,
+//         borderRightWidth: 0,
+//         borderLeftWidth: 0,
+//         borderStartColor: "red",
+//         borderWidth: 0.5,
+//         borderColor: '#d6d7da',
+//         display: "flex",
+//         flexDirection: "row",
+//         alignItems: "center",
+//         justifyContent: "space-between"
+//     },
+//     postTitle: {
+//         flexShrink: 1
+//     },
+//     postTitle: {
+//         flexShrink: 1
+//     },
+//     downloadIcon: {
+//         paddingHorizontal: 20,
+//         paddingVertical: 20
+//     }
+// });
--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import { FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { FlatList, StyleSheet, View} from 'react-native';
 
-import {downloadPost} from './helpers/Downloader';
+import Card from './Card';
 
 export default function Home(props) {
 
@@ -16,26 +15,15 @@ export default function Home(props) {
     async function getPosts() {
         setRefreshing(true);
         props.refreshing = true;
-        let data = await fetch("http://localhost:2368/ghost/api/v2/content/posts/?key=67fee21a30c345d85f0baf9264");
+        let data = await fetch("http://localhost:2368/ghost/api/v2/content/posts/?key=67fee21a30c345d85f0baf9264&include=tags");
         data = await data.json();
         setPosts(data.posts);
         setRefreshing(false);
     }
 
-    function ListItem(post) {
-        return (
-            <TouchableOpacity style={styles.listItem} onPress={() => {props.navigation.navigate("Episode", {content: post.data.html, title: post.data.title})}}>
-                <Text style={styles.postTitle}>{post.data.title}</Text>
-                <TouchableOpacity onPress={() => {downloadPost(post.data)}}>
-                    <Ionicons style={styles.downloadIcon} name="ios-add" size={25}/>
-                </TouchableOpacity>
-            </TouchableOpacity>
-        );
-    }
-
   return (
     <View style={styles.container}>
-        <FlatList refreshing={refreshing} onRefresh={getPosts} style={styles.list} data={posts} renderItem={({item}) => <ListItem data={item} />} keyExtractor={item => item.uuid}/>
+        <FlatList refreshing={refreshing} onRefresh={getPosts} style={styles.list} data={posts} renderItem={({item}) => <Card post={item} navigation={props.navigation}/>} keyExtractor={item => item.uuid}/>
     </View>
 
   );
@@ -45,27 +33,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
-  },
-  listItem: {
-    borderRadius: 4,
-    borderRightWidth: 0,
-    borderLeftWidth: 0,
-    borderStartColor: "red",
-    borderWidth: 0.5,
-    borderColor: '#d6d7da',
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  postTitle: {
-    flexShrink: 1
-  },
-  downloadIcon: {
-      paddingHorizontal: 20,
-      paddingVertical: 20
-  },
-  list: {
-      marginLeft: 20
   }
 });
